6c81ef3769c090d7c87be03d775c5cd72102e714
kathyshoo <113232687+kathyshoo@users.noreply.github.com>
2022-10-03 16:52:56 +0300

    Сделано: 1. Сложение 2. Умножение 3. Деление 4. Вычитание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //      Первое число
+        // запрос
+        System.out.print("Введите число: ");
+        Scanner input = new Scanner (System.in);
+        //присвоение
+        int chislo1 = input.nextInt();
+
+        //      Второе число
+        //запрос
+        System.out.print("Введите второе число: ");
+        Scanner input2 = new Scanner (System.in);
+        //присвоение
+        int chislo2 = input2.nextInt();
+
+        //      Действие
+        //запрос
+        System.out.print("Введите номер действия: ");
+        Scanner input3 = new Scanner (System.in);
+        int znak = input3.nextInt();
+
+        //сложние
+        if(znak==1){
+            System.out.print("Сумма чисел: " + (chislo1 + chislo2));
+        }
+        //умножение
+        if(znak==2){
+            System.out.print("Произведение чисел: " + (chislo1 * chislo2));
+        }
+        //деление
+        if(znak==3){
+            System.out.print("Частное чисел: " + (chislo1 / chislo2));
+        }
+        //вычитание
+        if(znak==4){
+            System.out.print("Разность чисел: " + (chislo1 - chislo2));
+        }
+
     }
 }
