c61a55f092c47c4f58340e53ec8d6c29633db3ec
kathyshoo <113232687+kathyshoo@users.noreply.github.com>
2022-10-04 20:30:57 +0300

    Исправлена поддержка вещественных чисел

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,16 @@ public class Main {
         //      Первое число
         // запрос
         System.out.print("Введите число: ");
-        Scanner input = new Scanner (System.in);
+        Scanner ain = new Scanner (System.in);
         //присвоение
-        double chislo1 = input.nextInt();
+        double a = ain.nextDouble();
 
         //      Второе число
         //запрос
         System.out.print("Введите второе число: ");
-        Scanner input2 = new Scanner (System.in);
+        Scanner bin = new Scanner (System.in);
         //присвоение
-        double chislo2 = input2.nextInt();
+        double b = bin.nextDouble();
 
         //      Действие
         //запрос
@@ -23,19 +23,19 @@ public class Main {
         int znak = input3.nextInt();
         //сложение
         if(znak==1){
-            System.out.print("Сумма чисел: " + (chislo1 + chislo2));
+            System.out.print("Сумма чисел: " + (a + b));
         }
         //умножение
         if(znak==2){
-            System.out.print("Произведение чисел: " + (chislo1 * chislo2));
+            System.out.print("Произведение чисел: " + (a * b));
         }
         //деление
         if(znak==3){
-            System.out.print("Частное чисел: " + (chislo1 / chislo2));
+            System.out.print("Частное чисел: " + (a / b));
         }
         //вычитание
         if(znak==4){
-            System.out.print("Разность чисел: " + (chislo1 - chislo2));
+            System.out.print("Разность чисел: " + (a - b));
         }
     }
 }
