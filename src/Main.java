bee1626590544622201f8dff3d42d053a83764b9
kathyshoo <113232687+kathyshoo@users.noreply.github.com>
2022-10-09 01:10:47 +0300

    Переписан код с использованием методов Добавлен цикл для повторного запуска калькулятора

--- src/Main.java
+++ src/Main.java
@@ -2,74 +2,85 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double c=0;
-        //      Действие
+        Scanner input = new Scanner(System.in);
         //пример
         System.out.println("1. a + b = ?");
         System.out.println("2. a * b = ?");
         System.out.println("3. a / b = ?");
         System.out.println("4. a - b = ?");
         System.out.println("5. ax² + bx + c = 0      x-?");
-        //запрос
-        System.out.print("Введите номер действия: ");
-        Scanner input = new Scanner (System.in);
-        int znak = input.nextInt();
 
-        //      Первое число
-        System.out.print("Введите a: ");
-        //Scanner ain = new Scanner (System.in);
-        double a = input.nextDouble();
-        //      Второе число
-        System.out.print("Введите b: ");
-        //Scanner bin = new Scanner (System.in);
-        double b = input.nextDouble();
-        if (znak==5) {//      Третье число (если уравнение)
-            System.out.print("Введите c: ");
-            //Scanner cin = new Scanner(System.in);
-            c = input.nextDouble();
+        int znak = 1;
+        while (znak != 0) {
+            //запрос знака
+            System.out.println("Если вы хотите выйти из программы введите число больше 5 или меньше 1");
+            znak = znak_();
+            if (znak != 0) {
+                //      Первое число
+                System.out.print("Введите a: ");
+                double a = input.nextDouble();
+                //      Второе число
+                System.out.print("Введите b: ");
+                double b = input.nextDouble();
+                if (znak == 5) {//      Третье число (если уравнение)
+                    System.out.print("Введите c: ");
+                    double c = input.nextDouble();
+                    urav(a, b, c);
+                } else {
+                    otvet(a, b, znak);
+                }
+            }
         }
+    }
 
-        //      Решение
-        //сложение
-        if(znak==1){
-            System.out.print("a + b = " + (a + b));
+    public static void otvet(double a, double b, int z) {
+        if (z == 1) {
+            System.out.println("a + b = " + (a + b) + "\n");
         }
         //умножение
-        else if(znak==2){
-            System.out.print("a * b = " + (a * b));
+        else if (z == 2) {
+            System.out.println("a * b = " + (a * b) + "\n");
         }
         //деление
-        else if(znak==3){
-            System.out.print("a / b = " + (a / b));
+        else if (z == 3) {
+            System.out.println("a / b = " + (a / b) + "\n");
         }
         //вычитание
-        else if(znak==4){
-            System.out.print("a - b " + (a - b));
+        else if (z == 4) {
+            System.out.println("a - b " + (a - b) + "\n");
         }
-        //квадратное уравнение
-        else if(znak==5){
-            //решение уравнения
-            double d = b * b - 4 * a * c;
-            if (d > 0){
-                //дескриминант>0
-                double x1 = (-1 * b - Math.sqrt(d)) / (2 * a);
-                double x2 = (-1 * b + Math.sqrt(d)) / (2 * a);
-                System.out.println("x1 = " + x1);
-                System.out.println("x2 = " + x2);
-            }
-            else if (d == 0){
-                //дескриминант=0
-                double x = (-1 * b) / (2 * a);
-                System.out.println("x = " + x);
-            }
-            else {
-                //дескриминант<0
-                System.out.println("Нет корней");
-            }
-        }
-        //другое число в знаке
         else {
-            System.out.println("Неверное действие");
+            System.out.println("Неверный знак" + "\n");
         }
+    }
+
+    public static void urav(double a, double b, double c){
+        double d = b * b - 4 * a * c;
+        if (d > 0){
+            //дескриминант>0
+            double x1 = (-1 * b - Math.sqrt(d)) / (2 * a);
+            double x2 = (-1 * b + Math.sqrt(d)) / (2 * a);
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        }
+        else if (d == 0){
+            //дескриминант=0
+            double x = (-1 * b) / (2 * a);
+            System.out.println("x = " + x);
+        }
+        else {
+            //дескриминант<0
+            System.out.println("Нет корней");
+        }
+    }
+
+    public static int znak_(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите номер действия: ");
+        int zn = input.nextInt();
+        if (zn < 1 | zn > 5){
+            zn = 0;
+        }
+        return (zn);
     }
 }
