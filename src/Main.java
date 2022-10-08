1f684a9d4cccf91edcb40472599dab816c6e907e
kathyshoo <113232687+kathyshoo@users.noreply.github.com>
2022-10-09 01:19:58 +0300

    Подправлен вывод в консоль и ввод переменных

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         int znak = 1;
         while (znak != 0) {
             //запрос знака
-            System.out.println("Если вы хотите выйти из программы введите число больше 5 или меньше 1");
+            System.out.println("Если вы хотите выйти из программы введите 0");
             znak = znak_();
             if (znak != 0) {
                 //      Первое число
@@ -22,6 +22,7 @@ public class Main {
                 //      Второе число
                 System.out.print("Введите b: ");
                 double b = input.nextDouble();
+                //проверка на уравнение
                 if (znak == 5) {//      Третье число (если уравнение)
                     System.out.print("Введите c: ");
                     double c = input.nextDouble();
@@ -35,22 +36,19 @@ public class Main {
 
     public static void otvet(double a, double b, int z) {
         if (z == 1) {
-            System.out.println("a + b = " + (a + b) + "\n");
+            System.out.println("Ответ: a + b = " + (a + b) + "\n");
         }
         //умножение
         else if (z == 2) {
-            System.out.println("a * b = " + (a * b) + "\n");
+            System.out.println("Ответ: a * b = " + (a * b) + "\n");
         }
         //деление
         else if (z == 3) {
-            System.out.println("a / b = " + (a / b) + "\n");
+            System.out.println("Ответ: a / b = " + (a / b) + "\n");
         }
         //вычитание
         else if (z == 4) {
-            System.out.println("a - b " + (a - b) + "\n");
-        }
-        else {
-            System.out.println("Неверный знак" + "\n");
+            System.out.println("Ответ: a - b " + (a - b) + "\n");
         }
     }
 
@@ -61,25 +59,29 @@ public class Main {
             double x1 = (-1 * b - Math.sqrt(d)) / (2 * a);
             double x2 = (-1 * b + Math.sqrt(d)) / (2 * a);
             System.out.println("x1 = " + x1);
-            System.out.println("x2 = " + x2);
+            System.out.println("x2 = " + x2 + "\n");
         }
         else if (d == 0){
             //дескриминант=0
             double x = (-1 * b) / (2 * a);
-            System.out.println("x = " + x);
+            System.out.println("x = " + x + "\n");
         }
         else {
             //дескриминант<0
-            System.out.println("Нет корней");
+            System.out.println("Нет корней" + "\n");
         }
     }
 
     public static int znak_(){
+        //запрос действия
         Scanner input = new Scanner(System.in);
         System.out.print("Введите номер действия: ");
         int zn = input.nextInt();
-        if (zn < 1 | zn > 5){
-            zn = 0;
+        //проверка на правильность номера действия
+        while (zn < 0 | zn > 5){
+            System.out.println("Если вы хотите выйти из программы введите 0");
+            System.out.print("Неверный номер действия, попробуйте ещё раз: ");
+            zn = input.nextInt();
         }
         return (zn);
     }
